066f208f3d0e60c7ce1bcb7a8ad5ecf543490000
Joseph Kerkhof <musicaljoeker@gmail.com>
2017-05-22 12:23:06 -0500

    Updates to the image positioning inside the footer 4 section

--- src/themes/uw-oshkosh-divi/footer.php
+++ src/themes/uw-oshkosh-divi/footer.php
@@ -94,10 +94,10 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 								</div>
 						</div> <!-- #footer-column-3 -->
 						<div id="footer-column-4" class="col">
-								<a href="http://www.ncahlc.org/?option=com_directory&amp;Action=ShowBasic&amp;instid=2030" target="_blank">
+								<a href="http://www.ncahlc.org/?option=com_directory&amp;Action=ShowBasic&amp;instid=2030" target="_blank" class="image-wrapper">
 									<img class="accredited-img" src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/images/footer/hlc.png') ?>" id="footer-accredited" class="inline" alt="Higher Learning Commission">
 								</a>
-								<a href="http://www.wisconsin.edu/" target="_blank">
+								<a href="http://www.wisconsin.edu/" target="_blank" class="image-wrapper">
 									<img class="uw-system-img" src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/images/footer/uw-system.png') ?>" id="footer-uw-logo" class="inline" alt="UW System">
 								</a>
 						</div> <!-- #footer-column-4 -->
