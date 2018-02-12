5c1da3c0ec71716c9ef4cfee9b54f9c7abeb2077
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-02-12 11:15:14 -0600

    Adding more 404 logic
    This update decreases the sensitivite for sending an email to the wordpress admin email and gets rid of many false positives.

--- 404.php
+++ 404.php
@@ -98,11 +98,22 @@
 							);
 						}
 
+						function giveHost($host_with_subdomain) {
+							$array = explode(".", $host_with_subdomain);
+						
+							return (array_key_exists(count($array) - 2, $array) ? $array[count($array) - 2] : "").".".$array[count($array) - 1];
+						}
+
 						#some variables for the script to use
 						#if you have some reason to change these, do.  but wordpress can handle it
-						$adminemail = get_option('admin_email'); #the administrator email address, according to wordpress
 						$website = get_bloginfo('url'); #gets your blog's url from wordpress
 						$websitename = get_bloginfo('name'); #sets the blog's name, according to wordpress
+						$referer_host = giveHost(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST));
+						$link = $website . $_SERVER['REQUEST_URI'];
+						$link_path = parse_url($link, PHP_URL_PATH);
+						$link_path_extension = pathinfo($link_path, PATHINFO_EXTENSION);
+						$link_query_string = parse_url($link, PHP_URL_QUERY);
+						$unimportant_file_extensions = array("bmp", "gif", "img", "jpe", "jpeg", "jpg", "png", "tiff", "ico", "css", "js", "map");
 
 						if (!isset($_SERVER['HTTP_REFERER'])) {
 							#politely blames the user for all the problems they caused
@@ -113,23 +124,30 @@
 							echo "clicked a link to";
 							
 							// setup a message to be sent to me
-							$user_agent = getBrowser();
-							$browser_string = $user_agent['name'] . " " . $user_agent['version'] . " on " .$user_agent['platform'];
-
-							$failure_message = "An invalid link was discovered on $websitename\n";
-							$failure_message .= "Invalid link: " . $website . $_SERVER['REQUEST_URI'] . "\n";
-							$failure_message .= "Referer: " . $_SERVER['HTTP_REFERER'] . "\n";
-							$failure_message .= "IP Address: " . $_SERVER['REMOTE_ADDR'] . "\n";
-							$failure_message .= "User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n";
-							$failure_message .= "User Browser: " . $browser_string . "\n\n";
-							$failure_message .= "This was not caused by user error, it was caused by a bad link in the webpage. It is highly suggested that you fix this bad link.";
-
-							mail('wordpressadmin@uwosh.edu', "Invalid Link Discovered On " . $websitename, $failure_message, "From: $websitename <noreply@$website>"); // email you about problem
-							
-							$casemessage = "An administrator has been emailed about this problem, too."; // set a friendly message
+							if( !in_array( strtolower( $link_path_extension ), $unimportant_file_extensions ) && 
+								$referer_host == "uwosh.edu"  &&
+								substr( $link_query_string , 0, 6) != "author" &&
+								( $_SERVER['REQUEST_URI'] != "/wp-login.php" || $_SERVER['REQUEST_URI'] != "//wp-login.php" ) ){ // the bad link was a page, and from a uw-oshkosh site, and 404 page isn't an author attack, and the IP isn't receiving a 404 because it's blocked
+									$user_agent = getBrowser();
+									$browser_string = $user_agent['name'] . " " . $user_agent['version'] . " on " .$user_agent['platform'];
+		
+									$failure_message = "An invalid link was discovered on $websitename\n";
+									$failure_message .= "Invalid link: " . $link . "\n";
+									$failure_message .= "Referer: " . $_SERVER['HTTP_REFERER'] . "\n";
+									$failure_message .= "IP Address: " . $_SERVER['REMOTE_ADDR'] . "\n";
+									$failure_message .= "User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n";
+									$failure_message .= "User Browser: " . $browser_string . "\n\n";
+									$failure_message .= "This was not caused by user error, it was caused by a bad link in the webpage. It is highly suggested that you fix this bad link.";
+		
+									mail("wordpressadmin@uwosh.edu", "Invalid Link Discovered On " . $websitename, $failure_message, "From: $websitename <noreply@$website>"); // email you about problem
+									
+									$casemessage = "An administrator has been emailed about this problem, too."; // set a friendly message
+							} else{
+								$casemessage = "We are sorry about that!"; // ¯\_(ツ)_/¯
+							}
 						}
 
-						echo " " . $website . $_SERVER['REQUEST_URI']; ?>. Unfortunately, this page doesn't exist. <?php echo $casemessage; ?>  You can click back 
+						echo " " . $link; ?>. Unfortunately, this page doesn't exist. <?php echo $casemessage; ?>  You can click back 
 						and try again or choose from the links below to help find what you are looking for. Or just <a href="<?php echo $website ?>">go home</a> and start over.
 					</p>
 					<br />
