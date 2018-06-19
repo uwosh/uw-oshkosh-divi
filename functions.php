8594d3d07b5287789b9cfbc96dd43883086e7011
SaraTabitha <mayhes06@uwosh.edu>
2018-06-19 09:22:08 -0500

    Accessibility audit: users needed to be able to zoom into the page; removed & replaced function in Divi theme that was preventing that. maximum-scale > 2 && user-scalable > 0

--- functions.php
+++ functions.php
@@ -77,4 +77,27 @@ function foobar_setup() {
   global $editor_styles;
   $editor_styles = array();
 }
+
+?>
+<?php
+/*
+* For accessibility a user needs to be able to zoom into a webpage, ex. cases where the user has poor eyesight, to acheive this
+* we need the meta tag in the header to have "maximum-scale" > 2 and "user-scalable" cannot be disabled.
+*/
+
+/*remove the function that sets:
+* '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />'
+*/
+function child_remove_parent_function(){
+    remove_action('wp_head', 'et_add_viewport_meta');
+}
+add_action('wp_loaded', 'child_remove_parent_function');
+
+/*
+* replace removed function with child function that allows zooming
+*/
+function et_add_viewport_meta_2(){
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />';
+}
+add_action('wp_head', 'et_add_viewport_meta_2');
 ?>
